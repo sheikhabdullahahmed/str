6727d5149a9d868d1dedeee5a0a5035c8327f3ae
sheikhabdullahahmed <sheikhabdullahahmed>
2024-03-12 02:01:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,192 @@
+// let a = prompt ("Enter your first nmuber");
+// let b = prompt ("Enter your second number");
+// let result = a+b;
+// console.log (result);
+
+// let userinp = prompt ("What is your Favorite Moblie");
+// console.log ("this is your favourit Mobile" + userinp +(userinp.length) );
+
+// let a =+prompt ("Enter Your First Number");
+// let b = +prompt ("Enter Your Second Number");
+// let result = a + b;
+// console.log (result)
+
+// let userinp = prompt ("What is Your Favorite Mobile")
+// console.log (`this is your ${userinp}`)
+
+
+let items = [250, 260, 270, 280, 290 , 300];
+let idex =  0 ;
+for (let val of items ) {
+    console.log (`value at index ${idex} = ${val}`)
+    idex++;
+}
+
+// let items = [20,30,40,50];
+// // let idex = 0;
+// for (let i = 0 ; i < items.length; i++) {
+//     console.log (`value at index is ${items}`)
+
+// }
+
+
+// let items = ["Sheikh Abdullah", "Sheikh", "Shahmeer", "Ahemd", "Ameen"];
+// let i = 0;
+// for (let value of  items) {
+//     console.log (`value at index ${i} = ${value}`)
+//     i++
+// }
+
+
+    // let items = ["Sheikh Abdullah", "Abdullah", "Hamza", "Shahzaib", "Wali"];
+    // let i = 0;
+    // for (let value of items) {
+    //     console.log (`value at index ${i} = ${value}`)
+    //     i++ 
+    // }
+
+    // let items = ["Sheikh Abdullah", "Ahmer", "Rafay"];
+    // let i = 0 ;
+    // for (let value of items) {
+    //     console.log (`value at index ${i} = ${value}`)
+    //     i++
+    // }
+
+
+    // let items = [10, 20, 30, 40, 50]
+    // let i = 0 ;
+    // for(let value of items) {
+    //     console.log (`value at index ${i} = ${value}`)
+    //     i++
+    // }
+
+// let items = ["Sheikh", "Ahmer", "Laptop", "Computer"];
+// let i = 0;
+// for (let value of items) {
+//     console.log (`value at index ${i} = ${value}`)
+//     i++
+// } 
+
+
+// let items =["Abdullah", "Sheikh", "Ahmer", "Laptop", "Computer"];
+// let i = 0; 
+// for (let value of items) {
+//     console.log (`value at index ${i} = ${value}`)
+//     i++
+// }
+
+// let items = [2,2,4,4,5,6,7,];
+// for (let i = 0; i <= items.length; i++) {
+//     let offer = items[i] / 10;
+//     items -= offer
+// }
+
+// console.log (items)
+
+// let items = [20,30,40,50];
+// let i = 0;
+// for (let value of items) {
+//     let offer = value / 5;
+//     items[i] = items[i] - offer;
+//     console.log (`value after offer = ${items[i]}`)
+//     i++
+// }
+
+
+// let items = [200, 300, 400, 500 ];
+// let i = 0;
+// for (let value of items) {
+//     let offer = value / 20;
+//     items[i] = items[i] - offer;
+//     console.log(`value after = ${items [i]}`)
+//    i++ 
+// } 
+
+
+// let items = [10,20,30,40];
+// let i = 0; 
+// for (let value of items) {
+//     let offer = value / 30;
+//     items [i] = items[i] - offer;
+//     console.log (`value after = ${items [i]}`)
+//     i++
+// }
+
+
+// let items = [10,20,30,40,50]
+// let i = 0;
+// for (let value of items) {
+//     let offer = value / 40;
+//     items[i] = items[i] - offer;
+//     console.log (`value after = ${items [i]}`)
+//     i++
+// } 
+
+
+// let items = [20,30,40,50,60]
+// let i = 0;
+// for (let value of items) {
+//     let offer = value / 20;
+//     items[i] = items[i] - offer
+//     console.log (`value after = ${items [i]}`)
+//     i++
+// }
+
+
+// let items = [200, 300,400,500,600];
+// let i = 0; 
+// for (let value of items) {
+//     let offer = value / 20;
+//     items [i] = items [i] - offer;
+//     console.log (`value after = ${items [i]}`)
+//     i++
+// }
+
+// let items = [100,20,30,40];
+// let i = 0 ;
+// for (let value of items) {
+//     let offer = value / 10;
+//     items [i] = items [i] - offer;
+//     console.log (`value after = ${items[i]}`)
+//     i++
+// }
+
+
+
+
+// let arry = ["Sheikh", "Abdullah"];
+// arry.splice("MATH","Sheikh")
+// console.log (arry)
+
+
+// let marval = ["Thor","Spiderman", "Ironamn"];
+// let Dc = ["Superman", "Batman",];
+// let herose = marval.concat(Dc);
+// console.log (herose);
+
+// let arry = ["Sheikh", "Abdullah", "Ahmer", "Shahmeer"];
+// let arry2 = ["Marvel", "DC"];
+// let concat = arry.concat(arry2);
+// console.log (concat)
+
+
+// let arry = ["Sheikh", "Abdullah", "Gamer","Google", "Microsoft" ];
+// arry.shift()
+// arry.splice(2, 1, "Ghar")
+// // arry.splice(3,3)
+// arry.push("Amazon")
+// console.log (arry)
+
+// let arry = ["Avengers","Sheikh billioner"];
+// arry.push("Gamer", "Amazon")
+// console.log (arry)
+
+
+// let cars = ["Supra", "G-Wagon", "Audi", "BMW"];
+// let i = 0 ;
+// for (let value of cars ) {
+//     console.log (`first index of the arry = ${i} ${value}`)
+
+//     i++
+// }
+
